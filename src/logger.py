a3af10f7bff26c82fd95ff5958b93d5545a9eaaa
Sergei Zobov <sergey@szobov.ru>
2024-08-21 06:50:55 +0200

    src/logger.py: cleanup logger

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,22 @@
 import logging
 import logging.config
-import pathlib
+import os
+from pathlib import Path
 
+import platformdirs
 import structlog
+from rich.traceback import install
+from structlog.typing import Processor
+
+logger = structlog.getLogger(__name__)
 
 
-def setup_logging(name: str) -> None:
-    pre_chain = [
+def get_log_level() -> int:
+    return int(os.environ.get("PYTHON_LOG_LEVEL", logging.INFO))
+
+
+def get_pre_chain() -> list[Processor]:
+    return [
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
         structlog.processors.StackInfoRenderer(),
@@ -22,8 +32,19 @@ def setup_logging(name: str) -> None:
         ),
     ]
 
-    log_dir = pathlib.Path("/tmp/logs")
-    log_dir.mkdir(parents=True, exist_ok=True)
+
+def get_log_file() -> Path:
+    log_dir = platformdirs.user_log_path("agent_joggler", ensure_exists=True)
+    log_file = log_dir / "agent_joggler.log"
+    return log_file
+
+
+def setup_logging(name: str) -> None:
+    install(show_locals=True)
+    pre_chain = get_pre_chain()
+    log_file = get_log_file()
+
+    logger.info("Logging to file", log_file=log_file)
 
     logging.config.dictConfig(
         {
@@ -55,8 +76,10 @@ def setup_logging(name: str) -> None:
                 },
                 "file": {
                     "level": "DEBUG",
-                    "class": "logging.handlers.WatchedFileHandler",
-                    "filename": "/tmp/logs/agent_joggler.log",
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "maxBytes": 1024 * 1024 * 10,
+                    "backupCount": 5,
+                    "filename": log_file,
                     "formatter": "plain",
                 },
             },
@@ -76,7 +99,7 @@ def setup_logging(name: str) -> None:
         + [
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
-        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        wrapper_class=structlog.make_filtering_bound_logger(get_log_level()),
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
