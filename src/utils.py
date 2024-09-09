fe17ee3ab0f500693c3fa30db02513ba39bcdc05
Sergei Zobov <sergey@szobov.ru>
2024-09-09 09:18:52 +0200

    src/utils.py: add helper function to convert env var to bool

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,17 @@
 import os
 
 
-def is_debug():
-    return os.environ.get("DEBUG", False)
+def env_var_to_bool(s: str | None) -> bool:
+    if s is None:
+        return False
+    truevals = ("yes", "y", "on", "true", "t", "1")
+    falsevals = ("no", "n", "off", "false", "f", "0")
+    if s.lower() in truevals:
+        return True
+    if s.lower() in falsevals:
+        return False
+    return True
+
+
+def is_debug() -> bool:
+    return env_var_to_bool(os.getenv("DEBUG"))
